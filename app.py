ca7fc4b04c186d1413df0fba8abaed1499b5960e
germainramanantsoa2-a11y <germainramanantsoa2@gmail.com>
2026-05-27 13:41:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -201,13 +201,4 @@ for i, t in enumerate(st.session_state.tasks):
                     save_tasks(st.session_state.tasks)
                     st.from flask import Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, ton app To-do marche !"
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
